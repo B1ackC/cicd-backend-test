ba5dfd22b811e8c253d72e63420bc66532108eb4
B1ackC <owlx113@gmail.com>
2025-11-11 22:16:27 +0900

    chore: dev 브랜치 CI/CD 동작 테스트

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,7 +2,6 @@ package com.example
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -10,7 +9,7 @@ fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         routing {
             get("/") {
-                call.respondText("CI-CD Hello, World!!")
+                call.respondText("Hello, World!!")
             }
         }
     }.start(wait = true)
